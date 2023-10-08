c174211776731f3a09a4411883b10d40e82f822b
shaoyuchu <r11922047@ntu.edu.tw>
2023-10-08 20:47:03 +0800

    test constructors and value type

--- TestJson.cpp
+++ TestJson.cpp
@@ -4,16 +4,55 @@
 
 CPPUNIT_TEST_SUITE_REGISTRATION(JsonTest);
 
+JsonTest::JsonTest() {
+    string str = string("Hello world!");
+    this->str = str;
+    this->charArr = new char[str.length() + 1];
+    strcpy(this->charArr, str.c_str());
+}
+
 void JsonTest::setUp() {
     this->nullJson1 = new Json();
     this->nullJson2 = new Json(nullptr);
+    this->doubleJson05 = new Json(0.5);
+    this->doubleJson25 = new Json(2.5);
+    this->intJson1 = new Json(1);
+    this->intJson5 = new Json(5);
+    this->boolJson0 = new Json(false);
+    this->boolJson1 = new Json(true);
+    this->strJson1 = new Json(string(""));
+    this->strJson2 = new Json(this->str);
+    this->strJson3 = new Json(this->charArr);
 }
 
-void JsonTest::nullConstructorTest() {
-    CPPUNIT_ASSERT(*(this->nullJson1) == *(this->nullJson2));
+void JsonTest::testNull1() { CPPUNIT_ASSERT(this->nullJson1->is_null()); }
+
+void JsonTest::testNull2() { CPPUNIT_ASSERT(this->nullJson2->is_null()); }
+
+void JsonTest::testDoubleNumber() { CPPUNIT_ASSERT(this->doubleJson05->is_number()); }
+
+void JsonTest::testIntNumber() { CPPUNIT_ASSERT(this->intJson1->is_number()); }
+
+void JsonTest::testBool() { CPPUNIT_ASSERT(this->boolJson0->is_bool()); }
+
+void JsonTest::testString() {
+    CPPUNIT_ASSERT(this->strJson1->is_string());
+    CPPUNIT_ASSERT(this->strJson2->is_string());
+    CPPUNIT_ASSERT(this->strJson3->is_string());
 }
 
 void JsonTest::tearDown() {
     delete this->nullJson1;
     delete this->nullJson2;
+    delete this->doubleJson05;
+    delete this->doubleJson25;
+    delete this->intJson1;
+    delete this->intJson5;
+    delete this->boolJson0;
+    delete this->boolJson1;
+    delete this->strJson1;
+    delete this->strJson2;
+    delete this->strJson3;
+
+    delete[] this->charArr;
 }
--- TestJson.hpp
+++ TestJson.hpp
@@ -3,22 +3,48 @@
 
 #include <cppunit/extensions/HelperMacros.h>
 
+#include <string>
+
 #include "json11.hpp"
+
+using namespace std;
 using namespace json11;
 
 class JsonTest : public CPPUNIT_NS::TestFixture {
     CPPUNIT_TEST_SUITE(JsonTest);
-    CPPUNIT_TEST(nullConstructorTest);
+    CPPUNIT_TEST(testNull1);
+    CPPUNIT_TEST(testNull2);
+    CPPUNIT_TEST(testDoubleNumber);
+    CPPUNIT_TEST(testIntNumber);
+    CPPUNIT_TEST(testBool);
+    CPPUNIT_TEST(testString);
     CPPUNIT_TEST_SUITE_END();
 
    public:
+    JsonTest();
     void setUp();
-    void nullConstructorTest();
+    void testNull1();
+    void testNull2();
+    void testDoubleNumber();
+    void testIntNumber();
+    void testBool();
+    void testString();
     void tearDown();
 
    private:
+    string str;
+    char* charArr;
     Json* nullJson1;
     Json* nullJson2;
+    Json* doubleJson05;
+    Json* doubleJson25;
+    Json* intJson1;
+    Json* intJson5;
+    Json* boolJson0;
+    Json* boolJson1;
+    Json* strJson1;
+    Json* strJson2;
+    Json* strJson3;
 };
 
 #endif
